820e8eff4d952f2d3aec974b5a51cf177dfe584a
josdoaitran <josdoaitran@gmail.com>
2018-07-16 17:08:20 +0700

    update ii

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -1,9 +1,15 @@
 package common;
 
+import org.testng.annotations.BeforeSuite;
+
 /**
  * Created by DoaiTran
  */
 public class BaseTest {
     public static String baseAPIUrl;
+    @BeforeSuite
+    public void beforeTest(){
+
+    }
 
 }
--- src/test/java/tests/getWeatherData.java
+++ src/test/java/tests/getWeatherData.java
@@ -1,5 +1,22 @@
 package tests;
 
-public class getWeatherData {
+import com.jayway.restassured.RestAssured;
+import common.BaseTest;
+import org.testng.annotations.Test;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 
+/**
+ * Example test with Rest Assert
+ */
+public class getWeatherData extends BaseTest{
+    @Test(description="Testing the web service whether success 200 has been received")
+    @Description ("Description : Testing the web service whether success 200 has been received")
+    @Severity (SeverityLevel.CRITICAL)
+    public void testGetWeatherDataFromALocation() {
+        RestAssured.baseURI = baseAPIUrl;
+
+    }
 }
+
